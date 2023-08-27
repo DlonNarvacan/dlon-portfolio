ceb6a55c5e0e75385fedee17b46ca451c6406352
Arnold Narvacan <arnoldnarvacan28@gmail.com>
2023-08-28 01:11:08 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Arnold Narvacan | Portfolio
+      Arnold Narvacan | Website Portfolio
     </main>
   );
 }
